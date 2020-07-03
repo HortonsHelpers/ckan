664a8d9d4378848d59f5a64c94204cf90fc95b87
william dutton <will.dutt@gmail.com>
2020-07-03 14:47:31 +1000

    Incorporate delete() and download() into IUploader, update ResourceUpload, Upload with functionality from controller resource_download to allow better cloud integrations
    - Add metadata to IUploader interface's
    - Add new resource_file_metadata_show get api to surface metadata checks, usage case is archiver or similar verifying files have not change or disappeared
    - Hook in delete uploader in resource delete action logic, use upload download logic fully
    - Remove unneeded code as its now in the ResourceUpload class, call upload delete when a resource is also deleted
    - Uploader and interface update so signature has filename
    - Allow filenames to be preserved in uploader
    - Add logging if delete method not found
    - Need package dict to call uploader correctly
    - Add metadata to uploader interface, next step, surface it via package…

--- ckan/tests/logic/action/test_get.py
+++ ckan/tests/logic/action/test_get.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+import __builtin__ as builtins
 import datetime
 import re
 
@@ -7,6 +8,7 @@ import copy
 import pytest
 from six import text_type
 from six.moves import xrange
+import mock
 
 from ckan import model
 import ckan.logic as logic
@@ -17,6 +19,8 @@ import ckan.tests.helpers as helpers
 import ckan.plugins.toolkit as tk
 from ckan import __version__
 from ckan.lib.search.common import SearchError
+
+from ckan.lib import uploader as ckan_uploader
 
 
 @pytest.mark.usefixtures("clean_db", "with_request_context")
@@ -2405,6 +2409,62 @@ class TestShowResourceView(object):
 
         with pytest.raises(logic.NotFound):
             helpers.call_action("resource_view_show", id="does_not_exist")
+
+
+def mock_open_if_open_fails(*args, **kwargs):
+    try:
+        return real_open(*args, **kwargs)
+    except (OSError, IOError):
+        return fake_open(*args, **kwargs)
+
+
+class ShowResourceFileMetadata(object):
+
+    @helpers.change_config('ckan.storage_path', '/doesnt_exist')
+    @mock.patch.object(builtins, 'open', side_effect=mock_open_if_open_fails)
+    @mock.patch.object(ckan_uploader, 'os', fake_os)
+    @mock.patch.object(ckan_uploader, '_storage_path', new='/doesnt_exist')
+    def test_resource_file_metadata_show_meta_data_returned(self, _):
+        user = factories.User()
+        pkg = factories.Dataset(creator_user_id=user['id'])
+
+        url = url_for(
+            controller='api',
+            action='action',
+            logic_function='resource_create', ver='/3')
+        env = {'REMOTE_USER': user['name'].encode('ascii')}
+        postparams = {
+            'name': 'test-flask-upload',
+            'package_id': pkg['id']
+        }
+        upload_content = 'test-content,and2'
+        upload_info = ('upload', 'test-upload.csv', upload_content)
+        app = self._get_test_app()
+        resp = app.post(
+            url, params=postparams,
+            upload_files=[upload_info],
+            extra_environ=env
+        )
+        result = resp.json['result']
+        eq('upload', result['url_type'])
+        eq(len(upload_content), result['size'])
+
+        metaresult = helpers.call_action('resource_file_metadata_show', id=result['id'])
+        eq(len(upload_content), metaresult['size'])
+        eq('text/csv', metaresult['content_type'])
+        eq('', metaresult['hash'])
+
+    def test_resource_file_metadata_show_id_missing(self):
+
+        nose.tools.assert_raises(
+            logic.ValidationError,
+            helpers.call_action, 'resource_file_metadata_show')
+
+    def test_resource_file_metadata_show_id_not_found(self):
+
+        nose.tools.assert_raises(
+            logic.NotFound,
+            helpers.call_action, 'resource_file_metadata_show', id='does_not_exist')
 
 
 class TestGetHelpShow(object):
